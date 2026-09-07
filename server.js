22480d880ae8d3119359b43c57adc4064fa6988d
cloudswebnepal8 <cloudswebnepal8@gmail.com>
2026-09-07 09:56:12 +0545

    files

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://anugraha-frontend-beta.vercel.app",
-    "https://anugraha-backend-94rc.vercel.app/api/admin/login"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
@@ -27,6 +26,11 @@ const corsOptions = {
 
 
 connectDB()
+
+
+seedAdmin().catch((error) => {
+  console.error("Seed admin error:", error);
+});
 
 
 
@@ -58,8 +62,8 @@ const PORT = process.env.PORT || 3001
 
 app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`)
-    console.log("EMAIL_USER:", process.env.EMAIL_USER)
-    console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "LOADED" : "NOT LOADED")
+    // console.log("EMAIL_USER:", process.env.EMAIL_USER)
+    // console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "LOADED" : "NOT LOADED")
 
-    await seedAdmin()
+    // await seedAdmin()
 })
